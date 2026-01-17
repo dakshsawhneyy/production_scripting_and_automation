c5ab38c1be207f5f968c69d7b611b1642c5f9f8d
Daksh Sawhney <darkstardaksh@gmail.com>
2026-01-17 10:28:57 +0530

    Python script for converting raw text into json object for better readibility

---
+++ Stale_Artifact_Auditor/analyzer.py
@@ -0,0 +1,44 @@
+import subprocess
+import os
+import json
+
+raw_file = "./raw_targets.txt"
+json_file = "./outputs.json"
+
+if not os.path.exists(raw_file):
+    print("raw paths file didn't found")
+    exit(1)
+
+data_list = []
+
+with open(raw_file, "r") as f:
+    for line in f:
+        clean_path = line.strip()
+        if clean_path and not clean_path.startswith("-"):
+            
+            raw = subprocess.run(["du", "-sk", clean_path], capture_output=True, text=True)
+            result = raw.stdout.strip()
+
+            payload = {
+                "filepath": result.split()[1],
+                "size": result.split()[0]
+            }
+            
+            data_list.append(payload)
+            
+# Create a JSON File and put name and size into that
+# if os.path.exists(json_file) and os.path.getsize(json_file) >= 0:
+#     with open(json_file, "r") as f:
+#         try:
+#             data = json.load(f)    
+#         except json.JSONDecodeError:
+#             data = []
+    
+#         data.extend(data_list)
+    
+#         with open(json_file, "w") as fi:
+#             fi.write(json.dumps(data, indent=4))
+
+# OverWrite Mode
+with open(json_file, "w") as f:
+    json.dump(data_list, f, indent=4)
